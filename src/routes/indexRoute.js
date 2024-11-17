917660f6393dbaed029b0bb103b09450122c2de5
xanh <nvxanh75@gmail.com>
2024-11-17 15:50:07 +0700

    link to shop.html ( product list done )

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,8 +1,10 @@
 const homeRouter = require("./homeRoute");
 const aboutRouter = require("./aboutRoute");
+const shopRoute = require("./shopRoute");
 
 function route(app) {
     app.use("/about.html", aboutRouter);
+    app.use("/shop.html", shopRoute);
     app.use("/", homeRouter);
 }
 
