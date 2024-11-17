96d3790397af03ca7dcc96a2374b7ba94d8ca6ba
xanh <nvxanh75@gmail.com>
2024-11-18 00:14:23 +0700

    fix passwprd user DB

--- databaseScript.sql
+++ databaseScript.sql
@@ -40,7 +40,7 @@ INSERT INTO Product VALUES
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password_1 VARCHAR(255) NOT NULL
 );
 INSERT INTO users (email, password) VALUES
 ('test@gmail.com', '123');
